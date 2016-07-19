5b435988227a99c8680b52fff514d84225c12a2d
adath325 <adath325@mds-xa201s-p003.vdi.student.otago.ac.nz>
2016-07-19 14:31:58 +1200

    Created Product domain class.

---
+++ admin/src/domain/Product.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author adath325
+ */
+public class Product {
+    private int productID;
+    private String name;
+    private String description;
+    private String category;
+    private int price;
+    private int quantity;
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", name=" + name + '}';
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
