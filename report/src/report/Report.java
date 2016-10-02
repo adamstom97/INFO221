021be6a7435d1cb20fb780b2bb6b2786003a43bc
adath325 <adath325@mds-xa201s-p003.vdi.student.otago.ac.nz>
2016-10-03 01:25:52 +1300

    Created a new project for generating reports.

---
+++ report/src/report/Report.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package report;
+
+/**
+ *
+ * @author adath325
+ */
+public class Report {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
