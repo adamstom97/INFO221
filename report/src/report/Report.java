87d795fb177b8222396e3e7aaaf95b466465a348
adath325 <adath325@mds-xa201s-p003.vdi.student.otago.ac.nz>
2016-10-03 01:52:54 +1300

    Integrated receipt into report project.

--- report/src/report/Report.java
+++ report/src/report/Report.java
@@ -5,6 +5,20 @@
  */
 package report;
 
+import dao.DaoException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
 /**
  *
  * @author adath325
@@ -15,7 +29,21 @@ public class Report {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String sql = "SELECT * FROM order_receipt";
+        try (Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost:9097/project;IFEXISTS=TRUE", "sa", "");
+                PreparedStatement stmt = con.prepareStatement(sql);) {
+            ResultSet rs = stmt.executeQuery();
+            try {
+            JasperPrint report = JasperFillManager.fillReport("h:/INFO221/reports/receipt.jasper",
+                            new HashMap<String, Object>(), new JRResultSetDataSource(rs));
+            JasperViewer.viewReport(report);
+            JasperExportManager.exportReportToPdfFile(report, "h:/INFO221/reports/receipt.pdf");
+            } catch (JRException ex) {
+                throw new DaoException(ex.getMessage(), ex);
+            }
+        } catch (SQLException ex) {
+            throw new DaoException(ex.getMessage(), ex);
+        }
     }
-    
+
 }
